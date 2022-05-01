3709dd39edfe7837ecbe3581b0d7b508f82d23ca
TheRealNoob <mike1118@live.com>
2022-05-01 16:47:56 -0500

    compress .sql LFS files

--- docker-entrypoint-initdb.d/1_create_databases.sql
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:022e26d992ca5e94704f374e4e3c486a3c7fee5dc3941a7f6ee5ab4137073118
-size 81
--- docker-entrypoint-initdb.d/2_playerdata.sql
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:67002e4420e07327631d7d4005a4ca443548eb0d9be05085b22df94cd7ecae0b
-size 184755915
